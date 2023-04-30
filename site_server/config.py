e90a8fd2ee3bc6037c1e7e98a4708e8faafefcb4
Maxim Odinokov <odi.max2002@gmail.com>
2023-04-30 17:27:14 +0300

    update config.py

--- site_server/config.py
+++ site_server/config.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 upl_folder = 'static/images'
 app.config['SECRET_KEY'] = '1SuperSecretKey1'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///' #доделать
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@127.0.0.1:5432/SIPI' #доделать
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
