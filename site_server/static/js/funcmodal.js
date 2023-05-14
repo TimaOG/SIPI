2c7e9e58e035ccdbc739587283f9e817b19b0636
Danil Egorkin <93032618+elmore17@users.noreply.github.com>
2023-05-14 15:00:42 +0300

    update index.html and js

---
+++ site_server/static/js/funcmodal.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector(".btn-toggle");
+const theme = document.querySelector("#theme-link");
+btn.addEventListener("click", function() {
+    if ( document.getElementById("flexSwitchCheckDefault").checked === true ) {
+        theme.href = "../static/css/dark_version.css";
+    } else {
+        theme.href = "../static/css/custom.min.css";
+    }
+});
