289ccf616bc430ad8e0d4c565d26859087a68164
AbaninTimofey <bookgun@mail.ru>
2023-06-04 21:53:19 +0300

    gang fix

--- site_server/static/js/analiz.js
+++ site_server/static/js/analiz.js
@@ -1,31 +1,3 @@
-// var checkbox = document.getElementById('flexCheckDefault_{{stock.code}}');
-// var xValues = histData.QIWI.Date;
-// var data1 = histData.QIWI.Close;
-// console.log(dataNames);
-// var chartOptions = {
-//     type: "line",
-//     data: {
-//         labels: xValues,
-//         datasets: [
-//             {
-//                 data: checkbox.checked ? data1 : [],
-//                 borderColor: "red",
-//                 fill: false
-//             }
-//         ]
-//     },
-//     options: {
-//         legend: {display: false}
-//     }
-// };
-
-// var chart = new Chart("myCharttt", chartOptions);
-//
-// checkbox.addEventListener('change', function () {
-//     chart.data.datasets[0].data = this.checked ? data1 : [];
-//     chart.update();
-// });
-
 var list = new Map;
 var chartOptions;
 var checkbox;
@@ -33,44 +5,69 @@ var current_id;
 var current_name;
 
 
-function getChart(name, id) {
-    current_id = id;
-    current_name = name;
-    list.set(current_id, histData[name]['Close'])
-    checkbox = document.getElementById(`flexCheckDefault_${name}`);
+//function getChart() {
+//    current_id = id;
+//    current_name = name;
+//    list.set(current_id, histData[name]['Close'])
+//    checkbox = document.getElementById(`flexCheckDefault_${name}`);
+//    chartOptions = {
+//        type: "line",
+//        data: {
+//            labels: histData[name]['Date'],
+//            datasets: [
+//                {
+//                    data: checkbox.checked ? list.get(current_id) : [],
+//                    borderColor: "red",
+//                    fill: false
+//                }
+//            ]
+//        },
+//        options: {
+//            legend: {display: false}
+//        }
+//    }
+//    //checkbox.addEventListener('change', function () {
+//    // });
+//}
+function getChart() {
+    var stock = document.getElementsByClassName("checkBoxStock")
+    var dataset = []
+    for(var i = 0; i < stock.length; i++) {
+        if(stock[i].checked) {
+            dataset.push({
+                data: histData[stock[i].name]['Close'],
+                borderColor: "red",
+                fill: false
+            })
+        }
+    }
     chartOptions = {
         type: "line",
         data: {
             labels: histData[name]['Date'],
-            datasets: [
-                {
-                    data: checkbox.checked ? list.get(current_id) : [],
-                    borderColor: "red",
-                    fill: false
-                }
-            ]
+            datasets: dataset
         },
         options: {
             legend: {display: false}
         }
     }
-    //checkbox.addEventListener('change', function () {
-    // });
+    var chart = new Chart("myCharttt", chartOptions);
+    chart.update();
 }
 
-var chart = new Chart("myCharttt", chartOptions);
-checkbox.addEventListener('change', function () {
-    chart.data.datasets[current_id].data = this.checked ? list.get(current_id) : [];
-    chart.update();
-});
-// if (checkbox) {
-//     checkbox.addEventListener('change', function () {
-//         chart.options = chartOptions;
-//         console.log(chartOptions);
-//         chart.update();
-//     });
-//     console.log(current_id, list.get(current_id));
-// }
+//var chart = new Chart("myCharttt", chartOptions);
+//checkbox.addEventListener('change', function () {
+//    chart.data.datasets[current_id].data = this.checked ? list.get(current_id) : [];
+//    chart.update();
+//});
+//// if (checkbox) {
+////     checkbox.addEventListener('change', function () {
+////         chart.options = chartOptions;
+////         console.log(chartOptions);
+////         chart.update();
+////     });
+////     console.log(current_id, list.get(current_id));
+//// }
 
 
 
