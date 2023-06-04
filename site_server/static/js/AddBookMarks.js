a10e44d90b41de2439ed09f9aa9b11318d5cf33d
Danil Egorkin <93032618+elmore17@users.noreply.github.com>
2023-06-04 17:34:10 +0300

    bookmarks

--- site_server/static/js/AddBookMarks.js
+++ site_server/static/js/AddBookMarks.js
@@ -1,11 +1,5 @@
-const items_bookmarks = document.querySelectorAll('.bookmarksItem');
-function AddBookMarks(){
-    for (var item in items_bookmarks){
-        if(document.getElementById("imgClickAndChange").src == "../static/icons/bookmark_dis.png"){
-            console.log("Yes");
-        }else{
-            document.getElementById("imgClickAndChange").src = "../static/icons/bookmark_active.png"
-        }
-        console.log(items_bookmarks[item]);
-    }
+function AddBookMarks(button){
+    var imgElement = button.querySelector('.imgClickAndChange');
+    imgElement.src = '../static/icons/bookmark_active.png';
+    return false;
 }
