1f7cf8abb1db40cf6ea44960f45bfa88082196f3
Maxim Odinokov <89403714+Tragica1@users.noreply.github.com>
2023-06-04 22:27:52 +0300

    hotfix

--- site_server/static/js/analiz.js
+++ site_server/static/js/analiz.js
@@ -1,51 +1,24 @@
-var list = new Map;
 var chartOptions;
-var checkbox;
-var current_id;
-var current_name;
 
-
-//function getChart() {
-//    current_id = id;
-//    current_name = name;
-//    list.set(current_id, histData[name]['Close'])
-//    checkbox = document.getElementById(`flexCheckDefault_${name}`);
-//    chartOptions = {
-//        type: "line",
-//        data: {
-//            labels: histData[name]['Date'],
-//            datasets: [
-//                {
-//                    data: checkbox.checked ? list.get(current_id) : [],
-//                    borderColor: "red",
-//                    fill: false
-//                }
-//            ]
-//        },
-//        options: {
-//            legend: {display: false}
-//        }
-//    }
-//    //checkbox.addEventListener('change', function () {
-//    // });
-//}
 function getChart() {
     var stock = document.getElementsByClassName("checkBoxStock")
-    var dataset = []
+    var dataset = [];
+    var colors = ["red", "green", "blue", "yellow", "purple", "black"];
     for(var i = 0; i < stock.length; i++) {
         if(stock[i].checked) {
             dataset.push({
                 data: histData[stock[i].name]['Close'],
-                borderColor: "red",
+                borderColor: colors[i],
                 fill: false
             })
         }
     }
+    console.log(dataset);
     chartOptions = {
         type: "line",
         data: {
-            labels: histData[name]['Date'],
-            datasets: dataset
+            labels: histData[stock[0].name]['Date'],
+            datasets: dataset,
         },
         options: {
             legend: {display: false}
@@ -54,20 +27,6 @@ function getChart() {
     var chart = new Chart("myCharttt", chartOptions);
     chart.update();
 }
-
-//var chart = new Chart("myCharttt", chartOptions);
-//checkbox.addEventListener('change', function () {
-//    chart.data.datasets[current_id].data = this.checked ? list.get(current_id) : [];
-//    chart.update();
-//});
-//// if (checkbox) {
-////     checkbox.addEventListener('change', function () {
-////         chart.options = chartOptions;
-////         console.log(chartOptions);
-////         chart.update();
-////     });
-////     console.log(current_id, list.get(current_id));
-//// }
 
 
 
