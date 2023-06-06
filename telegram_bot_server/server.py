9b4628b21285b795e747b983e4f26e5958562d76
TimaOG <bookgun@mail.ru>
2023-06-06 09:05:00 +0300

    update bot

--- telegram_bot_server/server.py
+++ telegram_bot_server/server.py
@@ -1,3 +1,4 @@
+import requests
 import telebot;
 import threading
 import time
@@ -10,29 +11,27 @@ conn = psycopg2.connect(dbname='SIPI', user='postgres',
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-  bot.send_message(message.chat.id,"""Чел... Ты теперь подписан на рассылку предсказаний акций раз день. 
+  bot.send_message(message.chat.id,"""Ты теперь подписан на рассылку предсказаний акций раз день. 
   Но для того чтобы она заработала нужно на сайте ввести свой телеграмм id\n
-  Еблуша, твой id кстати - """ + str(message.chat.id))
+  твой id кстати - """ + str(message.chat.id))
 
 
 def start_send_timer():
   while True:
     send_mailing()
-    time.sleep(60*60)
+    time.sleep(6)
 def send_mailing():
   global bot
   cursor = conn.cursor()
-  cursor.execute('SELECT id, telegramid FROM users WHERE sendmailing is true and telegramid is not null')
+  cursor.execute('SELECT id, telegramid FROM users WHERE users."isMailing" is true and telegramid is not null')
   records = cursor.fetchall()
   for el in records:
-    cursor.execute("""SELECT t2.stocksymbol FROM target_stocks as t1 LEFT JOIN stocks as t2 
+    cursor.execute("""SELECT t2.code FROM target_stocks as t1 LEFT JOIN stocks as t2 
     on t1.fkstock = t2.id WHERE t1.fkuser = %s""", (el[0], ));
     ts = cursor.fetchall()
     messageText = 'Прогноз акций на сегодня:\n'
     for elTs in ts:
-      messageText += elTs[0] + ' - 100p\n'
-    print(el[1])
-    print(messageText)
+      messageText += elTs[0] + ' - ' + str(requests.get(f'http://localhost:5001/prediction/{elTs[0]}').json()['Predictions']) + 'p\n'
     bot.send_message(int(el[1]), messageText)
   #print(records)
   cursor.close()
